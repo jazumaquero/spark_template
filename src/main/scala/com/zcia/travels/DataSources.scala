4c5e459915121da7884bd6eb4160fec7dfc5690b
José Antonio Zumaquero Torres <jazumaquer@gmail.com>
2022-11-21 18:30:55 +0100

    Change DataSources to implicitly import path of each datasource, to allow easy dependency injection.

--- src/main/scala/com/zcia/travels/DataSources.scala
+++ src/main/scala/com/zcia/travels/DataSources.scala
@@ -8,23 +8,27 @@ import scala.reflect.api.TypeTags
 import scala.reflect.runtime.universe.TypeTag
 
 trait DataSources {
+  final val DATE_FORMAT: String = "MM/dd/yyyy"
+  
+  implicit val fligthsPath: String
+  implicit val hotelsPath: String
+  implicit val usersPath: String
+
   implicit val spark: SparkSession
   import spark.implicits._
-
-  protected val dateFormat: String = "MM/dd/yyyy"
 
   protected def loadDatasetFromCsv[T <: Product: TypeTag](path: String): Dataset[T] = {
     val schema: StructType = Encoders.product[T].schema
     val ds: Dataset[T] = spark.read
       .schema(schema)
       .option("header", "true")
-      .option("dateFormat", dateFormat)
+      .option("dateFormat", DATE_FORMAT)
       .csv(path)
       .as[T]
     ds
   }
 
-  def fligths(path: String): Dataset[Flight] = loadDatasetFromCsv(path = path)
-  def hotels(path: String): Dataset[Hotel]   = loadDatasetFromCsv(path = path)
-  def users(path: String): Dataset[User]     = loadDatasetFromCsv(path = path)
+  def fligths: Dataset[Flight] = loadDatasetFromCsv(path = fligthsPath)
+  def hotels: Dataset[Hotel]   = loadDatasetFromCsv(path = hotelsPath)
+  def users: Dataset[User]     = loadDatasetFromCsv(path = usersPath)
 }
--- src/test/scala/com/zcia/travels/DataSourcesSpec.scala
+++ src/test/scala/com/zcia/travels/DataSourcesSpec.scala
@@ -9,24 +9,21 @@ import com.zcia.travels.DataSources
 class DataSourcesSpec extends AnyFlatSpec with Matchers with SparkSessionSuite with DataSources {
     protected def testResourcePath(resource: String) : String = getClass.getResource(f"/traveldataset/$resource").toString
     
-    protected final val fligthsPath : String = testResourcePath("fligths")
-    protected final val hotelsPath : String = testResourcePath("hotels")
-    protected final val usersPath : String = testResourcePath("users")
+    val fligthsPath : String = testResourcePath("fligths")
+    val hotelsPath : String = testResourcePath("hotels")
+    val usersPath : String = testResourcePath("users")
     
-    protected final val fligthsCount: Integer= 271888
-    protected final val hotelsCount: Integer= 40552
-    protected final val usersCount: Integer= 1340
-    
-    //protected val datasources = new DataSources(spark)
-    //protected val spark = this.spark()
+    val fligthsCount: Integer= 271888
+    val hotelsCount: Integer= 40552
+    val usersCount: Integer= 1340   
 
     "Fligths data source" should f"must include $fligthsCount rows" in {               
-        this.fligths(fligthsPath).count shouldEqual fligthsCount
+        this.fligths.count shouldEqual fligthsCount
     }
     "Hotesls data source" should f"must include $hotelsCount rows" in {               
-        this.hotels(hotelsPath).count shouldEqual hotelsCount
+        this.hotels.count shouldEqual hotelsCount
     }
     "Users data source" should f"must include $usersCount rows" in {               
-        this.users(usersPath).count shouldEqual usersCount
+        this.users.count shouldEqual usersCount
     }
 }
