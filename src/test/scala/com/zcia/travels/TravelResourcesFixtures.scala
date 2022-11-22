e0ea21b311d8535ed5349f739cc73354feba14a1
José Antonio Zumaquero Torres <jazumaquer@gmail.com>
2022-11-22 20:46:19 +0100

    Add base statistics example.

--- src/test/scala/com/zcia/travels/TravelResourcesFixtures.scala
+++ src/test/scala/com/zcia/travels/TravelResourcesFixtures.scala
@@ -3,7 +3,7 @@ package com.zcia.travels
 trait TravelResourcesFixtures {
     protected def testResourcePath(resource: String) : String = getClass.getResource(f"/traveldataset/$resource").toString
 
-    val fligthsPath : String = testResourcePath("fligths")
-    val hotelsPath : String = testResourcePath("hotels")
-    val usersPath : String = testResourcePath("users")
+    final val fligthsPath : String = testResourcePath("fligths")
+    final val hotelsPath : String = testResourcePath("hotels")
+    final val usersPath : String = testResourcePath("users")
 }
